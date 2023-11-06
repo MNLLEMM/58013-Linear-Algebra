060cf904784f729784b466565074bf4f6fb74a11
MNLLEMM <143975539+MNLLEMM@users.noreply.github.com>
2023-11-06 08:15:50 +0800

    Updated MIDTERM.m

--- MIDTERM.m
+++ MIDTERM.m
@@ -1,14 +1,8 @@
-%Finding the determinant of the given matrix
-A = [-5 -1 -4; 4 0 -3; 2 -2 6];
-
-determinant_A = det(A);
-
-disp(determinant_A);
-
 %Square of A
 A = [1 2; 3 4];
 C = A^2;
 
+disp('A^2 is equal to: ')
 disp(C);
 
 %A multiplied by B
@@ -18,7 +12,29 @@ B = [1;2;3;4];
 
 Cab=A*B;
 
+disp('A*B is equal to: ')
 disp(Cab)
+
+%System of Equations
+SideA= [3, -5, 4; 5, 2, 1; 2, 3, -2];
+
+% Right-hand side vector b
+SideB = [3.4; 8.8; 19.2];
+
+% Solve the system of equations using the linsolve function
+x_linsolve = linsolve(SideA, SideB);
+
+disp('Answer for the system of linear equation: ')
+disp(x_linsolve);
+
+%C = A+B
+A = [0 1; 1 0];
+B = 2;
+CpB = A + B;
+
+disp('A + B is equal to: ')
+disp(CpB);
+
 
 %System of Equations
 SideA= [3, -5, 4; 5, 2, 1; 2, 3, -2];
